623a40998734cd210b76832c71afc0f0811be35a
Shariq Shahbaz <iamsh4r10@gmail.com>
2020-01-05 01:19:17 +0100

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+    var dos = document.querySelector("#dos");
+    var tres = document.querySelector("#tres");
+    dos.style.animationName = "animateIllustration2";
+    tres.style.animationName = "animateIllustration3";
+
+
+    document.querySelector(".icon").addEventListener("click", () =>{
+        document.querySelector(".icon").classList.toggle("active");
+    });
+    
+    var checkbox = document.querySelector('#switch');
+    checkbox.addEventListener("change", function () {
+        if (this.checked) {
+            trans()
+            document.documentElement.setAttribute("data-theme", "dark");
+        }
+        else {
+            trans()
+            document.documentElement.setAttribute("data-theme", "light");
+        }
+    })
+    
+    let trans = () => {
+        document.documentElement.classList.add('transition');
+        window.setTimeout(() => {
+            document.documentElement.classList.remove('transition');
+        }, 800)
+    }
+    
+    let themeConfigure = x =>{
+        if(x.matches){
+            document.documentElement.setAttribute("data-theme", "dark");
+            document.getElementById("switch").checked="true";
+        }
+        else{
+            document.documentElement.setAttribute("data-theme", "light");
+            document.getElementById("switch").checked = "false";
+        }
+    }
+
+    const x = window.matchMedia("(prefers-color-scheme: dark)");
+    themeConfigure(x);
+    x.addListener(themeConfigure)
+}
